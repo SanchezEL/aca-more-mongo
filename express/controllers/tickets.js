cb666fa000dddbd561bf8dcc0c7bb175b6bcbf79
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-06-13 20:58:46 -0500

    initial push

--- express/controllers/tickets.js
+++ express/controllers/tickets.js
@@ -3,13 +3,30 @@ var ObjectId = mongoose.Types.ObjectId
 const { TicketsModel } = require('../../mongo/models')
 
 // 1. create a function called getAll that returns everything
-
+const getAll = () => {
+  return TicketsModel.find({})
+}
 // 2. create a function called createMovie that accepts a "movie" param
-
+const createMovie = (movie) => {
+  return TicketsModel.create(movie)
+}
 // 3. create a function called deleteMovie that accepts a "movieName" param
+const deleteMovie = (movie) => {
+  // use name as the query 
+  return TicketsModel.findOneAndDelete({movieName: movie})
+}
 
 // 4. create a function called getById that accepts an "id" param and finds one ticket
 // hint: in your db query, you will use objectId like this: ObjectId(id)
+const getById = (id) => {
+  // console.log("so")
+  return TicketsModel.findOne({_id:id})
+}
 
 
-module.exports = {}
+module.exports = {
+  getAll,
+  createMovie,
+  deleteMovie,
+  getById
+}
